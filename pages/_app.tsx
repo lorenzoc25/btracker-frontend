a9734d8426d8a938d2a26178e80fce19d950e073
lorenzoc25 <lorenzocao25@gmail.com>
2021-11-06 20:12:07 -0700

    Create prototype of NavBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Nav from '../components/Nav';
 import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) =>  {
   return (
     <ChakraProvider>
+      <Nav />
       <Component {...pageProps} />
     </ChakraProvider>
   );
-}
+};
 
 export default MyApp;
