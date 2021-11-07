2245a72428aa4dc155b69005c7b4d038c2bc3adf
Xiaoyang Liu <siujoeng.lau@gmail.com>
2021-11-06 21:27:33 -0700

    Setup Color Mode Manager to detect system color mode
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,30 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  localStorageManager,
+} from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const config : ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
+
+const App = ({
+  Component,
+  pageProps,
+}: AppProps) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider
+      colorModeManager={localStorageManager}
+      theme={theme}
+    >
       <Component {...pageProps} />
     </ChakraProvider>
   );
-}
+};
 
-export default MyApp;
+export default App;
