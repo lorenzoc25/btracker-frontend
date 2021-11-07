9363fdd994477094fc9844eee9783edfbc92045f
Xiaoyang Liu <siujoeng.lau@gmail.com>
2021-11-06 22:48:48 -0700

    Setup Context API for state management (#5)

---
+++ context/reducer.ts
@@ -0,0 +1,14 @@
+import { Action, State } from '../types/context';
+
+export const reducer = (
+  state: State,
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'ADD':
+      break;
+    
+    default:
+      break;
+  }
+};
