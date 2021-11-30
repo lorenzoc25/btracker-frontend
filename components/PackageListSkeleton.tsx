d9890d06d115dc329e1005aadb3e8be0ae0fc80c
Xiaoyang Liu <siujoeng.lau@gmail.com>
2021-11-29 23:16:14 -0800

    Add skeleton component to Details page
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- components/PackageListSkeleton.tsx
+++ components/PackageListSkeleton.tsx
@@ -3,20 +3,24 @@ import { Skeleton, VStack } from '@chakra-ui/react';
 const PackageListSkeleton = () => (
   <VStack
     spacing={6}
+    my="2em"
     py={2}
     px={{ base: 4, md: 35, lg: 50 }}
   >
     <Skeleton
       w="full"
       height="128px"
+      rounded="md"
     />
     <Skeleton
       w="full"
       height="128px"
+      rounded="md"
     />
     <Skeleton
       w="full"
       height="128px"
+      rounded="md"
     />
   </VStack>
 );
--- pages/detail/[tracking].tsx
+++ pages/detail/[tracking].tsx
@@ -4,25 +4,30 @@ import { useEffect, useState, useContext } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import { useToast } from '@chakra-ui/react';
+
 import { AppContext } from '../../context/context';
 import Nav from '../../components/Nav';
 import Details from '../../components/Details';
 import { Package } from '../../types/package';
+import PackageListSkeleton from '../../components/PackageListSkeleton';
 
 const Tracking: NextPage = () => {
   const { state } = useContext(AppContext);
   const router = useRouter();
   const { tracking } = router.query;
+  const [loading, setLoading] = useState(false);
   const [packageInfo, setPackageInfo] = useState<Package>({} as Package);
   const toast = useToast();
 
   useEffect(() => {
     const getTrackingInfo = async () => {
+      setLoading(true);
       try {
         const packageItem = await axios.get<Package>(
           `/tracking/${tracking}`,
         );
         setPackageInfo(packageItem.data);
+        setLoading(false);
       } catch (error) {
         if (axios.isAxiosError(error)) {
           toast({
@@ -44,19 +49,36 @@ const Tracking: NextPage = () => {
     getTrackingInfo();
   }, [tracking]);
 
+  const renderDetails = () => {
+    if (loading) {
+      return (
+        <PackageListSkeleton />
+      );
+    }
+
+    if (tracking === undefined) {
+      return false;
+    }
+
+    return (
+      <Details
+        item={packageInfo}
+      />
+    );
+  };
+
   return (
     <>
       <Head>
         <title>BTracker</title>
       </Head>
 
-      <Nav isLoggedIn={state.token !== ''} />
+      <Nav
+        isLoggedIn={state.token !== ''}
+      />
+
       <div>
-        {tracking && (
-          <Details
-            item={packageInfo}
-          />
-        )}
+        {renderDetails()}
       </div>
     </>
   );
